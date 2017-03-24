0a417d4a657d6e7bd527cc32b8dfe33f10b9971e
Antonia Calia-Bogan <alfriadox@gmail.com>
2017-03-24 00:31:07 -0400

    created out file, need to fix other stuff

--- BlackRose/src/main.rs
+++ BlackRose/src/main.rs
@@ -6,17 +6,26 @@ use std::fs::File;
 use std::io::{self, Read, Write};
 fn main(){
     let args: Vec<String> = env::args().collect();
-    if args.len() > 2 {
+    if args.len() > 3 {
         let arglenstring = (args.len()-1).to_string();
-        let error_info = "Expected 1 argument, received ".to_string()+&arglenstring+" arguments.";
+        let error_info = "Expected 2 arguments, received ".to_string()+&arglenstring+" arguments.";
         errors::execute(errors::Error{error_type: "ArgumentError", line_num: 0, filename:"command", loc: 0, line: &args.join(" "), info: &error_info });
-    } else if args.len() == 2 {
+    } else if args.len() == 3 {
         let mut file = match File::open(&args[1]) {
             Ok(file) => file,
             Err(e) => {
                 let error_info_orig = &args[1];
                 let error_info = error_info_orig.to_string() + " could not be opened as a valid file!";
                 errors::execute(errors::Error{error_type: "ArgumentError", line_num: 0, filename:&args[1], loc: 0, line: &args.join(" "), info: error_info.as_str()});
+                panic!(e);
+            },
+        };
+        let mut out_file = match File::open(&args[2]) {
+            Ok(out_file) => out_file,
+            Err(e) => {
+                let error_info_orig = &args[2];
+                let error_info = error_info_orig.to_string() + " could not be opened as a valid file!";
+                errors::execute(errors::Error{error_type: "ArgumentError", line_num: 0, filename:&args[2], loc: 0, line: &args.join(" "), info: error_info.as_str()});
                 panic!(e);
             },
         };
@@ -30,17 +39,23 @@ fn main(){
                 panic!(e);
             },
         };
-        run_file(contents, &args[1]);
+        run_file(contents, out_file, &args[1]);
     } else {
         let prompt = vec!["radon"];
         run_prompt(&prompt);
     }
 }
 
-fn run_file(s: String, filename: &str) {
+fn run_file(s: String, mut output: File, file_name: &str) {
     let p = preproc::preprocessor(&s);
     for i in p {
-        println!("{}: {}", i.line_num, i.line);
+        match output.write(i.line.as_bytes()) {
+            Ok(val) => val,
+            Err(e) => {
+                errors::execute(errors::Error { error_type: "WriteOutError", line_num: i.line_num, filename: file_name, loc: 0, line: i.line, info: "ByteCode contained invalid characters!" });
+                panic!(e);
+            },
+        }
     }
 }
 
@@ -60,7 +75,7 @@ fn run_prompt(inlist: &[&str]) {
         match io::stdin().read_line(&mut buffer) {
             Ok(a) => a,
             Err(e) => {
-                errors::execute(errors::Error{error_type: "InvalidCharacterError", line_num: current_line, filename:"", loc: 0, line: &buffer.as_str(), info: (buffer.as_str().to_string()+ " contained invalid charcters!").as_str()});
+                errors::execute(errors::Error{error_type: "InvalidCharacterError", line_num: current_line, filename:"", loc: 0, line: &buffer.as_str(), info: (buffer.as_str().to_string()+ " contained invalid characters!").as_str()});
                 panic!(e);
             },
         };
