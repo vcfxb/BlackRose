38f0b933bcf847e3ffdb9cc4f55cfb2a231c4c14
Antonia Calia-Bogan <alfriadox@gmail.com>
2017-04-04 22:42:48 -0400

    More Work on lexer
    Lexer near completeed at this point.

--- BlackRose/src/lexer.rs
+++ BlackRose/src/lexer.rs
@@ -36,6 +36,22 @@ pub fn lex_line(cl: Vec<u8>) -> Vec<Vec<char>> {
                 };
             }
         }
+        else if character == '\'' { // Past here is essentially a match statement, but with more power to it
+            work_list.push(character);
+            loop {
+                match char_list.pop() {     // stop at end of line
+                    Some(n) => {
+                        if n == '\'' {
+                            work_list.push(n);
+                            break;
+                        } else {
+                            work_list.push(n);
+                        }
+                    },
+                    None => break,
+                };
+            }
+        }
         else if character == ' ' {}
         else if character.is_numeric() {      //is_numeric hopefully doesnt let in letters
             work_list.push(character);
@@ -71,9 +87,103 @@ pub fn lex_line(cl: Vec<u8>) -> Vec<Vec<char>> {
                 };
             }
         }
-        else if ['(', ')', '.', ';', '{', '}', ':'].contains(&character) {
+        else if ['(', ')', '.', ';', '{', '}', ':', ','].contains(&character) {
             work_list.push(character);
-        } else {
+        }
+            // Special characters
+        else if character == '=' {
+            work_list.push(character);
+            match char_list.pop() {     // stop at end of line
+                Some(n) => {
+                    if n == '=' {
+                        work_list.push(n);
+                    } else {
+                        char_list.push(n);     // if not one of the desired characters here, put it back
+                        break;
+                    }
+                },
+                None => break,
+            };
+        }
+        else if character == '-' {
+            match char_list.pop() {     // stop at end of line
+                Some(n) => {
+                    if ['=', '>', '-'].contains(&n) {
+                        work_list.push(n);
+                    } else {
+                        char_list.push(n);     // if not one of the desired characters here, put it back
+                        break;
+                    }
+                },
+                None => break,
+            };
+        }
+        else if character == '+' {
+                match char_list.pop() {     // stop at end of line
+                    Some(n) => {
+                        if ['=', '+'].contains(&n) {
+                            work_list.push(n);
+                        } else {
+                            char_list.push(n);     // if not one of the desired characters here, put it back
+                            break;
+                        }
+                    },
+                    None => break,
+                };
+            }
+        else if character == '!' {
+                match char_list.pop() {     // stop at end of line
+                    Some(n) => {
+                        if n == '=' {
+                            work_list.push(n);
+                        } else {
+                            char_list.push(n);     // if not one of the desired characters here, put it back
+                            break;
+                        }
+                    },
+                    None => break,
+                };
+        }
+        else if character == '<' {
+            match char_list.pop() {     // stop at end of line
+                Some(n) => {
+                    if n == '=' {
+                        work_list.push(n);
+                    } else {
+                        char_list.push(n);     // if not one of the desired characters here, put it back
+                        break;
+                    }
+                },
+                None => break,
+            };
+        }
+        else if character == '>' {
+                match char_list.pop() {     // stop at end of line
+                    Some(n) => {
+                        if n == '=' {
+                            work_list.push(n);
+                        } else {
+                            char_list.push(n);     // if not one of the desired characters here, put it back
+                            break;
+                        }
+                    },
+                    None => break,
+                };
+            }
+        else if character == '*' {
+                match char_list.pop() {     // stop at end of line
+                    Some(n) => {
+                        if n == '=' {
+                            work_list.push(n);
+                        } else {
+                            char_list.push(n);     // if not one of the desired characters here, put it back
+                            break;
+                        }
+                    },
+                    None => break,
+                };
+            }
+        else {
             work_list.push(character);
         }
         if !(work_list.is_empty()) {        // only push if not empty
@@ -88,85 +198,3 @@ pub struct LexedLine<'b> {
     pub line: Vec<Vec<char>>,
     pub original_line: &'b str,
 }
-
-
-// def oldlex(linelist):
-//     final = []
-//     for x, line in enumerate(linelist):
-//         temp = ''
-//         stemp = []
-//         fmode = False
-//         smode = False
-//         for y, char in enumerate(line):
-//             if smode == True:
-//                 if char in ["'", '"']:
-//                     stemp.append(char)
-//                     smode = False
-//                     final.append(''.join(stemp))
-//                     stemp = []
-//                     continue
-//                 else:
-//                     stemp.append(char)
-//                     continue
-//             elif smode == False:
-//                 if char in ["'", '"']:
-//                     stemp.append(char)
-//                     smode = True
-//                     continue
-//                 else:
-//                     pass
-//             if 48 <= ord(char) <= 57:
-//                 temp = temp + char
-//                 if 48 <= ord(line[y+1]) <= 57:
-//                     continue
-//                     continue
-//                 else:
-//                     if fmode == False:
-//                         continue
-//                 else:
-//                     if fmode == False:
-//                         final.append(int(temp))
-//                         temp = ''
-//                     elif fmode == True:
-//                         final.append(float(temp))
-//                         temp = ''
-//                         fmode = False
-//             elif char == '.':
-//                 if (48 <= ord(line[y-1]) <= 57) & (48 <= ord(line[y+1]) <= 57):
-//                     temp = temp+char
-//                 else:
-//                     final.append(char)
-//             elif (0x41 <= ord(char) <= 0x5A) | (0x61 <= ord(char) <= 0x7A):
-//                 temp = temp+char
-//                 if (0x41 <= ord(line[y+1]) <= 0x5A) | (0x61 <= ord(line[y+1]) <= 0x7A):
-//                     continue
-//                 else:
-//                     if temp == 'True':
-//                         final.append(True)
-//                     elif temp == 'False':
-//                         final.append(False)
-//                     else:
-//                         final.append(temp)
-//                     temp = ''
-//             elif char == ' ':
-//                 continue
-//             elif char == '\n':
-//                 continue
-//             else:
-//                 final.append(char)
-//     return final
-//
-// def lex(enumeratedlines):
-//     worklist = []
-//     final = []
-//     for pair in enumeratedlines:
-//         for x in pair[1].split(';'):
-//             worklist.append([pair[0], x])
-//     for n, pair in enumerate(worklist):
-//         if pair[1] in ['\n','']:
-//             worklist[n] = None
-//     for x in range(worklist.count(None)):
-//         del worklist[worklist.index(None)]
-//     for
-//     final = worklist
-//     return worklist
